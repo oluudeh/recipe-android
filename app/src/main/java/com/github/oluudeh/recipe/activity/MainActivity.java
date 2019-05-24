1d3fd95aa7f5c57b5ee7de16f2210f76da086c7a
ikay <oluudeh@gmail.com>
2019-05-24 06:46:59 +0100

    Initial commit

---
+++ app/src/main/java/com/github/oluudeh/recipe/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.github.oluudeh.recipe.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.oluudeh.recipe.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
